a3363bde705c2a33ec1dfa6e1514a42ff073d639
zkuokuo <1216819573@qq.com>
2019-01-11 15:42:18 +0800

    提交代码

---
+++ EngineLib/src/main/java/com/example/nibiru/nibiruengineapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nibiru.nibiruengineapp;
+
+//import android.os.Bundle;
+//
+//import x.core.XBaseVRActivity;
+//import x.core.ui.XBaseScene;
+//
+//public class MainActivity extends XBaseVRActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
