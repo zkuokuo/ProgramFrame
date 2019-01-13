75f09cd241e4834f8d2be6e3e8825af9dd2d922f
zkk <1216819573@qq.com>
2019-01-13 10:26:09 +0800

    测试提交

--- app/src/main/java/com/nibiru/programframe/dag/component/ApplicationComponent.java
+++ app/src/main/java/com/nibiru/programframe/dag/component/ApplicationComponent.java
@@ -3,7 +3,9 @@ package com.nibiru.programframe.dag.component;
 import com.nibiru.programframe.dag.module.ApplicationModule;
 import com.nibiru.programframe.data.source.DataManager;
 import com.nibiru.programframe.data.source.dbdata.DBHelper;
+
 import javax.inject.Singleton;
+
 import dagger.Component;
 
 @Singleton
@@ -11,4 +13,6 @@ import dagger.Component;
 public interface ApplicationComponent {
     DBHelper dbHelper();
     DataManager dataManager();
+
+
 }
